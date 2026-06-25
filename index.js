2a592e498abc1b8de3e2b9fede932a5df9de176f
Qutaiba-Alnoty <alrefaeequtiba010@gmail.com>
2026-06-25 22:36:20 +0300

    Implement Discord bot with vision test command

---
+++ index.js
@@ -0,0 +1,149 @@
+const {
+Client,
+GatewayIntentBits,
+Events,
+SlashCommandBuilder,
+REST,
+Routes,
+ActionRowBuilder,
+ButtonBuilder,
+ButtonStyle
+} = require("discord.js");
+
+
+const TOKEN = process.env.TOKEN;
+const CLIENT_ID = "1519773601438503002";
+
+
+const client = new Client({
+ intents:[
+  GatewayIntentBits.Guilds,
+  GatewayIntentBits.GuildMembers
+ ]
+});
+
+
+// slash command
+const commands = [
+ new SlashCommandBuilder()
+ .setName("visiontest")
+ .setDescription("Take your Vision test")
+].map(c=>c.toJSON());
+
+
+const rest = new REST({version:"10"})
+.setToken(TOKEN);
+
+
+(async()=>{
+ await rest.put(
+ Routes.applicationCommands(CLIENT_ID),
+ {body:commands}
+ );
+ console.log("Commands loaded");
+})();
+
+
+
+client.on(Events.GuildMemberAdd, async member=>{
+
+ let role =
+ member.guild.roles.cache.find(
+ r=>r.name==="Unawakened Traveler"
+ );
+
+ if(role){
+  await member.roles.add(role);
+ }
+
+});
+
+
+
+client.on(Events.InteractionCreate, async interaction=>{
+
+
+if(interaction.isChatInputCommand()){
+
+if(interaction.commandName==="visiontest"){
+
+
+let row = new ActionRowBuilder()
+.addComponents(
+
+new ButtonBuilder()
+.setCustomId("electro")
+.setLabel("Ambition ⚡")
+.setStyle(ButtonStyle.Primary),
+
+
+new ButtonBuilder()
+.setCustomId("pyro")
+.setLabel("Passion 🔥")
+.setStyle(ButtonStyle.Danger),
+
+
+new ButtonBuilder()
+.setCustomId("anemo")
+.setLabel("Freedom 🌪")
+.setStyle(ButtonStyle.Success)
+
+);
+
+
+await interaction.reply({
+content:"✨ Choose your path ✨",
+components:[row],
+ephemeral:true
+});
+
+}
+
+}
+
+
+
+if(interaction.isButton()){
+
+
+let result = {
+ electro:"⚡ Electro Vision",
+ pyro:"🔥 Pyro Vision",
+ anemo:"🌪 Anemo Vision"
+};
+
+
+let role =
+interaction.guild.roles.cache.find(
+r=>r.name===result[interaction.customId]
+);
+
+
+let old =
+interaction.guild.roles.cache.find(
+r=>r.name==="Unawakened Traveler"
+);
+
+
+await interaction.member.roles.add(role);
+
+if(old)
+await interaction.member.roles.remove(old);
+
+
+
+await interaction.reply(
+`✨ A Vision has descended from Celestia! ✨
+
+Congratulations ${interaction.user}
+You received ${result[interaction.customId]}`
+);
+
+
+}
+
+});
+
+
+
+client.login(TOKEN);
