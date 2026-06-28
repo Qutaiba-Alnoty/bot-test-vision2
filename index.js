9c97373de0ce15469422b140c8827618518b50e5
Qutaiba-Alnoty <alrefaeequtiba010@gmail.com>
2026-06-28 15:53:41 +0300

    Add HTTP server to respond with 'Bot is alive!'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
-
+ 
 http.createServer((req,res)=>{
   res.writeHead(200);
   res.end("Bot is alive!");
